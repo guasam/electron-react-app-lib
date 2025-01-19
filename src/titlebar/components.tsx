4b5a48157d799fc20144c8cba626d02cd277c6c4
guasam <guasamos@outlook.com>
2025-01-19 18:54:03 +0530

    Handle click outside closer for menu popup

--- src/titlebar/components.tsx
+++ src/titlebar/components.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { useWindowContext } from '..';
 import type { TitlebarMenu, TitlebarMenuItem } from './menus';
 import { TitlebarContextProvider, useTitlebarContext } from './provider';
@@ -38,18 +39,63 @@ const TitlebarMenu = () => {
 
 const TitlebarMenuItem = ({ menu, index }: { menu: TitlebarMenu; index: number }) => {
   const { activeMenuIndex, setActiveMenuIndex } = useTitlebarContext();
+  const menuItemRef = useRef<HTMLDivElement | null>(null);
 
   const togglePopup = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
-    setActiveMenuIndex(activeMenuIndex === index ? null : index);
+
+    // Check if the current menu item is the active one
+    if (activeMenuIndex === index) {
+      menuItemRef.current?.classList.remove('active');
+      setActiveMenuIndex(null);
+    }
+    // If the menu item is not active, activate it
+    else if (!menuItemRef.current?.classList.contains('active')) {
+      setActiveMenuIndex(index);
+      menuItemRef.current?.classList.add('active');
+    }
   };
 
-  const handleMouseOver = () => activeMenuIndex != null && setActiveMenuIndex(index);
+  const handleMouseOver = () => {
+    if (activeMenuIndex != null) {
+      setActiveMenuIndex(index);
+    }
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      menuItemRef.current &&
+      !menuItemRef.current.contains(event.target as Node) &&
+      menuItemRef.current.classList.contains('active')
+    ) {
+      setActiveMenuIndex(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (activeMenuIndex !== index) {
+      menuItemRef.current?.classList.remove('active');
+    } else {
+      menuItemRef.current?.classList.add('active');
+    }
+  }, [activeMenuIndex]);
 
   return (
-    <div className='titlebar-menuItem'>
-      <div className='menuItem-label' onClick={(e) => togglePopup(e)} onMouseOver={handleMouseOver}>
+    <div className='titlebar-menuItem' ref={menuItemRef}>
+      <div
+        className='menuItem-label'
+        onClick={(e) => togglePopup(e)}
+        onMouseOver={handleMouseOver}
+        onMouseDown={(e) => e.preventDefault()}
+      >
         {menu.name}
       </div>
       {activeMenuIndex === index && <div className='menuItem-popup'>This is good name</div>}
