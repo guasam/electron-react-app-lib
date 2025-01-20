5ba70203e24627400ebb52cedc094f9e1a1aaf94
guasam <guasamos@outlook.com>
2025-01-20 15:08:55 +0530

    Fix production build and enable splitting

--- build.ts
+++ build.ts
@@ -2,9 +2,10 @@ import type { BuildConfig } from 'bun';
 import dts from 'bun-plugin-dts';
 import './src/styles.css';
 import './src/titlebar/components';
+import './src/menus';
 
 const defaultBuildConfig: BuildConfig = {
-  entrypoints: ['./src/index.tsx', './src/window.ts', './src/menus.ts'],
+  entrypoints: ['./src/index.tsx', './src/menus.ts', './src/window.ts'],
   outdir: './dist',
   external: ['react', 'electron'],
 };
@@ -14,6 +15,7 @@ await Promise.all([
     ...defaultBuildConfig,
     plugins: [dts()],
     format: 'esm',
+    splitting: true,
     naming: {
       entry: '[dir]/[name].[ext]',
       asset: '[dir]/[name].[ext]',
